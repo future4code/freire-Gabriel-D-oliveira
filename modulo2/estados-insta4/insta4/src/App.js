17a3c62bac446ae8e81bda873edede84a7c6b455
Gabs27martins <gabriel27mdoliveira@gmail.com>
2022-06-08 17:16:22 -0300

    exercicio insta4

---
+++ modulo2/estados-insta4/insta4/src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import styled from 'styled-components'
+import Post from './components/Post/Post';
+
+const MainContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`
+
+class App extends React.Component {
+  render() {
+    return (
+      <MainContainer>
+        <Post
+          nomeUsuario={'paulinha'}
+          fotoUsuario={'https://picsum.photos/50/51'}
+          fotoPost={'https://picsum.photos/200/150'}
+        />
+        <Post
+          nomeUsuario={'Lukas Silva'}
+          fotoUsuario={'https://picsum.photos/50/53'}
+          fotoPost={'https://picsum.photos/200/155'}
+        />
+        <Post
+          nomeUsuario={'Marina Santos'}
+          fotoUsuario={'https://picsum.photos/50/55'}
+          fotoPost={'https://picsum.photos/200/154'}
+        />
+
+      </MainContainer>
+    );
+  }
+}
+
+export default App;
