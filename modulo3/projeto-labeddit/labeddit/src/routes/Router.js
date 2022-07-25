bf7aa8f65ea9b6cc7127d5ab733413a49843e283
Gabs27martins <gabriel27mdoliveira@gmail.com>
2022-07-25 18:07:59 -0300

    organizando os arquivos

---
+++ modulo3/projeto-labeddit/labeddit/src/routes/Router.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Login } from "../pages/LoginPage/Login";
+import { Register } from "../pages/RegisterPage/Register";
+import { Feed } from "../pages/FeedPage/Feed";
+import { Post } from "../pages/PostPage/Post";
+import { Error } from "../pages/ErrorPage/Error";
+
+export const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Login />} />
+        <Route path={"/register"} element={<Register />} />
+        <Route path={"/feed"} element={<Feed />} />
+        <Route path={"/feed/:post"} element={<Post />} />
+        <Route path={"*"} element={<Error />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
