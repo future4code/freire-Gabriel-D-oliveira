34ab0d80b861abcd5e296c4cc55da809d057f408
Gabs27martins <gabriel27mdoliveira@gmail.com>
2022-08-17 21:35:08 -0300

    exercicio fluxo-de-dados-backend

---
+++ modulo4/fluxo-de-dados-backend/src/data.ts
@@ -0,0 +1,6 @@
+export const products = [
+  { id: "1", name: "Colírio", price: 22 },
+  { id: "2", name: "Mouse", price: 100 },
+  { id: "3", name: "Caderno", price: 12 },
+  { id: "4", name: "Mochila", price: 50 },
+];
